2edd06e97b9f1dd298f1a4418b045b3f28923a1f
Ilamal <Ilari.s.Malinen@gmail.com>
2017-04-20 12:36:21 +0300

    R

--- Breakout/src/breakout/Pelaaja.java
+++ Breakout/src/breakout/Pelaaja.java
@@ -4,24 +4,24 @@ package breakout;
 import static breakout.peli.pelaaja;
 import static breakout.peli.peliAlusta;
 import javafx.geometry.Bounds;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 
 /**
  *  Pelaaja-luokka joka toimii pelaaja luokkana. Omaa laudan jota pelaaja liikuttelee ja se kimmottaa palloa.
+ *  staattinen luokka ei muuttuvia arvoja luonnissa
  * @author Ilari
  */
 public class Pelaaja {
     Rectangle lauta;
     static double sijaintiX=peli.WINDOWLEVEYS/2.0-42;   //haetaan asetettava X koordinaatti pelaajalle, jotta se tulisi keskelle ruutua. KOVAKOODATTU ARVO POISTA
-    static int sijaintiY=485;
-    static float speed=4;
-    Bounds pelirajat;
+    static int sijaintiY=485;   //kiva positio pelaajalle
+    static float speed=4;   //Turha hiiri liikkumisessa
+    Bounds pelirajat;   //ikkunan rajat
     
     int leveys=70;
     int korkeus =10;
     public Pelaaja() {       
-        this.lauta = new Rectangle(sijaintiX,sijaintiY,leveys,korkeus);     // luo laudan eli suorakulmion, kovakoodatuilla arvoilla
+        this.lauta = new Rectangle(sijaintiX,sijaintiY,leveys,korkeus);     // luo laudan eli suorakulmion, kovakoodatuilla arvoilla, jota sitten liikutellaan
     }
     /**
      * Liikuttaa pelaajan lautaa käytäjän hiiren mukana. Jos hiiri menee liian kauas lopetetaan seuraaminen.
