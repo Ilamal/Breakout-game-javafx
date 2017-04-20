73f583d1b6897ab11faffebcc3f74297047cb0b4
Ilamal <Ilari.s.Malinen@gmail.com>
2017-04-20 12:35:54 +0300

    Ready

--- Breakout/src/breakout/Pisteet.java
+++ Breakout/src/breakout/Pisteet.java
@@ -5,22 +5,25 @@
  */
 package breakout;
 
+import java.io.Serializable;
 import javafx.geometry.VPos;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
+import java.io.Serializable;
 
 /**
- *
+ * Pisteet olio pelaajan pisteen laskentaan ja nimen tallentamiseen
  * @author Ilari
  */
-public class Pisteet {
-    Text pisteTeksti;
+public class Pisteet implements Serializable,Comparable<Pisteet>{
+    static Text pisteTeksti;
     public int pisteet;
     String pelaajaNimi;
-
+    /**
+     * Luodaan pisteolio ja sen teksti, joka näkyy pelin ylänurkassa
+     */
     public Pisteet() {
         pisteTeksti = new Text("0");
         pisteTeksti.setFont(Font.font("Arial", FontWeight.BOLD, 50));
@@ -29,23 +32,52 @@ public class Pisteet {
         pisteTeksti.setTextOrigin(VPos.TOP);
         pisteTeksti.setLayoutX(pisteTeksti.getLayoutX()+(peli.WINDOWLEVEYS-100));
     }
-
+    /**
+     * palauttaa pistetekstin
+     * @return 
+     */
     public Text getPisteTeksti() {
         return pisteTeksti;
     }
-
+    /**
+     * palauttaa pisteet
+     * @return 
+     */
     public int getPisteet() {
         return pisteet;
     }
+    /**
+     * asettaa pisteet (ei suositella)
+     * @param pisteet 
+     */
     public void setPisteet(int pisteet) {
         this.pisteet = pisteet;
     }
+    /**
+     * updatettaa pisteteksin ajantasalle
+     */
     public void Update(){
         pisteTeksti.setText(String.valueOf(pisteet));
     }
+    /**
+     * Lisää tietyn verran pisteteitä ja päivittää pistetekstin. Käytä aina ennemmin kuin setPisteet.
+     * @param pisteet 
+     */
     public void LisaaPiste(int pisteet){
         this.pisteet+=pisteet;
         Update();
     }
+    /**
+     * uusi vertailu metodi, jotta voidaan asettaa oliot järjestykseen pisteiden perusteella.
+     * @param o
+     * @return 
+     */
+    @Override
+    public int compareTo(Pisteet o) {
+         return this.getPisteet()-o.getPisteet();
+    }
+
+   
     
+
 }
